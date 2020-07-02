e03fea07ed4b100a6037c8a1cb94fe8a28824df1
rinqu-eu <rinqu@interia.eu>
2020-07-02 04:31:10 +0100

    color: add to_RGB
    Converts hex string to a table of 0-1 values used by LOVE.

--- console/color.lua
+++ console/color.lua
@@ -1,6 +1,22 @@
 local utf8 = require(console.path_req .. ".utf8")
 local color = {}
 
+function color.to_RGB(hex_string)
+	local len  = hex_string:len()
+	assert(len == 7 or len == 9, "hex string expected, #rrggbb or #rrggbbaa")
+
+	local r = tonumber(hex_string:sub(2, 3), 16) / 255
+	local g = tonumber(hex_string:sub(4, 5), 16) / 255
+	local b = tonumber(hex_string:sub(6, 7), 16) / 255
+	local a
+
+	if (len == 9) then
+		a = tonumber(hex_string:sub(8, 9), 16) / 255
+	end
+
+	return {r, g, b, a}
+end
+
 function color.parse(text)
 	local parsed = {}
 
