4a8317591c06af7dd6f323d4e37bb01e750c9fb1
rinqu-eu <rinqu@interia.eu>
2020-07-08 18:28:29 +0100

    util: fixup

--- console/util.lua
+++ console/util.lua
@@ -4,19 +4,20 @@ function util.clamp(value, min, max)
 	assert(type(value) == "number", "arg #1 number expected, got " .. type(value))
 	assert(type(min) == "number", "arg #2 number expected, got " .. type(min))
 	assert(type(max) == "number", "arg #3 number expected, got " .. type(max))
+
 	if (value > max) then
 		return max
 	elseif (value < min) then
 		return min
-	else
-		return value
 	end
+
+	return value
 end
 
 function util.to_RGB_table(hex_string)
 	assert(type(hex_string) == "string", "arg #1 string expected, got " .. type(hex_string))
 	assert(hex_string:len() == 7 or hex_string:len() == 9, "arg #1 hex string expected, got _" .. hex_string .. "_")
-	assert(string.find(hex_string, "#%x%x%x%x%x%x") ~= nil or string.find(hex_string, "#%x%x%x%x%x%x%x%x") ~= nil, "arg #1 hex string expected, got _" .. hex_string .. "_")
+	assert(hex_string:find("#%x%x%x%x%x%x") ~= nil or hex_string:find("#%x%x%x%x%x%x%x%x") ~= nil, "arg #1 hex string expected, got _" .. hex_string .. "_")
 
 	local r = tonumber(hex_string:sub(2, 3), 16) / 255
 	local g = tonumber(hex_string:sub(4, 5), 16) / 255
